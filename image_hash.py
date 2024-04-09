bb2f2a172e83dbe8e3f4d7922d57970e72aff291
Eva Richter <eva.richter@jelos.de>
2024-04-09 18:09:26 -0400

    fixed errors

--- image_hash.py
+++ image_hash.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image, ImageOps
 import numpy as np
+import cv2
+
 
 def ahash(image, hash_size=8):
     image = ImageOps.grayscale(image)
@@ -10,11 +12,22 @@ def ahash(image, hash_size=8):
     diff = pixels > avg
     return ''.join(['1' if val else '0' for val in diff.flatten()])
 
-def dhash(image, hash_size=8):
+# def dhash(image, hash_size=8):
+#     image = ImageOps.grayscale(image)
+#     image = image.resize((hash_size + 1, hash_size), Image.Resampling.LANCZOS)
+#     pixels = np.array(image)
+#     diff = pixels[:, 1:] > pixels[:, :-1]
+#     return ''.join(['1' if val else '0' for val in diff.flatten()])
+
+def phash(image, hash_size=8):
+
     image = ImageOps.grayscale(image)
-    image = image.resize((hash_size + 1, hash_size), Image.Resampling.LANCZOS)
-    pixels = np.array(image)
-    diff = pixels[:, 1:] > pixels[:, :-1]
+    image = image.resize((32, 32), Image.Resampling.LANCZOS)
+    pixels = np.array(image, dtype=np.float32)
+    dct = cv2.dct(pixels)
+    dct_low_freq = dct[:hash_size, :hash_size]
+    median = np.median(dct_low_freq)
+    diff = dct_low_freq >= median
     return ''.join(['1' if val else '0' for val in diff.flatten()])
 
 def hamming_distance(hash1, hash2):
@@ -27,12 +40,12 @@ def main():
 
     st.title("Image Hashing App")
     st.write("Upload two images to check the how the two hashing algorithms compare. Also check with similar images, like screenshots or cropped images. ")
-    hashing_option = st.radio("Choose the hashing method:", ["Simple hash", "Difference hash"])
+    hashing_option = st.radio("Choose the hashing method:", ["Simple hash", "Perceptual hash"])
     if hashing_option == "Simple hash":
             st.write("Performing a simple hash: ")
 
-    elif hashing_option == "Difference hash":
-            st.write("Performing a difference hash: ")
+    elif hashing_option == "Perceptual hash":
+            st.write("Performing a perceptual hash: ")
 
     # Creating two columns for the file uploaders:
     col1, col2 = st.columns(2)
@@ -55,10 +68,10 @@ def main():
             st.write("Performing a simple hash: ")
             hash1 = ahash(image1)
             hash2 = ahash(image2)
-        elif hashing_option == "Difference hash":
-            st.write("Performing a difference hash: ")
-            hash1 = dhash(image1)
-            hash2 = dhash(image2)
+        elif hashing_option == "Perceptual hash":
+            st.write("Performing a perceptual hash: ")
+            hash1 = phash(image1)
+            hash2 = phash(image2)
 
         distance = hamming_distance(hash1, hash2)
         similarity = "similar" if distance <= 5 else "not similar"
